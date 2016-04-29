7ed21173df2c77298d4bf3498b0129fa46e4451a
yong.xu <yong.xu@Ctrip.com>
2016-04-29 16:05:01 +0800

    flyweight code.

--- flyweight.js
+++ flyweight.js
@@ -1,1 +1,14 @@
-var 
+var objectPoolFactory = function(createObjFn){
+    var objectPool = [];
+    return {
+       create:function(){
+        var obj = objectPool.length === 0 ? createObjFn.apply(this,arguments) : objectPool.shift();
+	return obj;
+    },
+    recover:function(obj){
+        objectPool.push(obj);
+    }
+    }
+}; 
+
+
