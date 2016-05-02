42912bad7a0901f942dd94f72b880258f1ac7efa
yong.xu <yong.xu@Ctrip.com>
2016-05-02 17:40:30 +0800

    mediator pattern.

--- mediator.js
+++ mediator.js
@@ -1,1 +1,25 @@
-var 
+var mediator = (function(){
+    var colorSel,memorySel;
+    return{
+        changed:function(obj){
+	    var color = colorSel.value,
+	        memory = memorySel.value;
+	    if(obj == colorSel){
+	       //...
+	    }else if(obj == memorySel){
+	      //...
+	    }
+	    if(!color){
+	      //...
+	      return;
+	    }
+	    if(!memory){
+	     //...
+	     return;
+	    }
+	}
+    }
+})(); 
+colorSel.onchange = function(){
+   mediator.changed(this);
+};
