8872b2c19a92271dac8924d1d51bb16debd398f2
yong.xu <yong.xu@Ctrip.com>
2016-05-02 17:49:01 +0800

    decorator pattern.

--- decorator.js
+++ decorator.js
@@ -1,1 +1,15 @@
-var 
+Function.prototype.before = function(beforefn){
+    var _self = this;
+    return function(){
+        beforefn.apply(this, arguments);
+	 return _self.apply(this,arguments);
+    }
+}; 
+Function.prototype.after = function(afterfn){
+    var _self = this;
+    return function(){
+        var ret = _self.apply(this, arguments);
+	afterfn.apply(this,arguments);
+	return ret;
+    }
+}
