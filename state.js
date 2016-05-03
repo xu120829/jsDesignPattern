9b85ae211f8f45919c1e370b3aeb229b1e7dda37
yong.xu <yong.xu@Ctrip.com>
2016-05-03 20:59:47 +0800

    state pattern.

--- state.js
+++ state.js
@@ -1,1 +1,31 @@
-var 
+var light = function(){
+    this.currState = FSM.off;
+    this.button = null;
+}; 
+light.prototype.init = function(){
+    var button = document.createElement("button"),
+        self = this;
+    button.innerHTML = "closed.";
+    this.button = document.body.appendChild(button);
+    this.button.onclick = function(){
+        self.surrState.buttonWasPressed.call(self);
+    }
+};
+var FSM = {
+   off:{
+      buttonWasPressed:function(){
+         console.log("close light.");
+	 this.button.innerHTML = "next .";
+	 this.currState = FSM.on;
+      }
+   },
+   on:{
+       buttonWasPressed:function(){
+          console.log("open light.");
+	  this.button.innerHTML = "next .";
+	  this.currState = FSM.off;
+       }
+   }
+};
+var lightObj = new light();
+lightObj.init();
