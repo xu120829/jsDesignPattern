26ef92d455b414a2cede422d1a3e4c60362fe280
yong.xu <yong.xu@Ctrip.com>
2016-05-05 19:05:23 +0800

    adapter pattern.

--- adapter.js
+++ adapter.js
@@ -1,1 +1,23 @@
-var  adapter = function(){};
+var  getXCity = function(){
+    var xCity = [
+       {
+         name:'cityname',
+	 id:1
+       },
+       {
+         name:'cityname',
+	 id:2
+       }
+    ];
+    return xCity;
+};
+var cityAdapter = function(){
+    var address = {},
+        oldaddress = {};
+    for(var i=0,c;c=oldaddress[i++];){
+       address[c.name]= c.id;
+    }
+    return function(){
+        return address;
+    }
+}
