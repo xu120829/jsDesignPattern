3754a99c64b49ab60d9c76a762d82054a19d0a65
yong.xu <yong.xu@Ctrip.com>
2016-05-05 19:28:00 +0800

    respon chain pattern.

--- responChain.js
+++ responChain.js
@@ -1,1 +1,18 @@
-var  resp = function(){}
+var  getActiveUploadObj = function(){
+    try{
+        return new ActiveXObject("TXFTNActiveX.FTNUpload");
+    }catch(e){
+        return "nextSuccessor";
+    }
+};
+var getFlashUploadObj = function(){
+    if(supportFlash()){
+       var str = '<object type="application/x-shockwave-flash"></object>';
+       return $(str).appendTo($('body'));
+    }
+    return 'nextSuccessor';
+};
+var getFormUploadObj = function(){
+    return '';
+};
+var getUploadObj = '';
