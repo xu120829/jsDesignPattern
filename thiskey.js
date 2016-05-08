fe543abf6ffb77b54efe8c6e3668b1f701632fe2
yong.xu <yong.xu@Ctrip.com>
2016-05-08 16:41:49 +0800

    update thiskey

--- thiskey.js
+++ thiskey.js
@@ -1,1 +1,46 @@
-var 
+//this keyword using.
+//one:as method of object
+var obj = {
+    num:1,
+    getNum:function(){
+       console.log( this === obj ); // true
+       console.log(this.num);//1
+    }
+}; 
+obj.getNum();
+//two:as normal function
+window.name = "globalName";
+var getName = function(){
+    return this.name;
+};
+console.log(getName());// globalName
+or
+window.name = "globalName";
+var myObject = {
+   name:'sven', 
+   getName:function(){
+       return this.name;
+   }
+};
+var getName = myObject.getName;
+console.log(getName());//globalName
+
+//html code.
+<html>
+ <body>
+   <div id='div1'>I am a div.</div>
+ </body>
+ <script>
+   window.id='window';
+   document.getElementById('div1').onclick = function(){
+      console.log(this.id);// div1
+      var callback = function(){
+          console.log(this.id);// window
+      }
+      callback();
+   };
+ </script>
+</html>
+
+
+
