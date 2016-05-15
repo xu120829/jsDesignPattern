da74defda310b3f8890c5ae060dd9dea9c798887
xu <xu090411@qq.com>
2016-05-15 15:15:38 +0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -36,3 +36,24 @@ for(var i = 0, len = nodes.length; i < len; i++){
 </script>
 </body>
 </html>
+
+// type
+var Type = {};
+for(var i = 0, type; type = ['String','Array','Number'][i++];){
+    (function(type){
+        Type['is'+type] = function(obj){
+            return Object.prototype.toString.call(obj) === '[object ' + type + '']';
+        }
+    })(type)
+};
+Type.isArray([]);
+Type.isString("str");
+
+
+
+
+
+
+
+
+
