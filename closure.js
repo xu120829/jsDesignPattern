e8dcd6d96aceb36377acc8e95d5cb15ed79a9def
xu <xu090411@qq.com>
2016-05-15 14:59:28 +0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,5 +1,6 @@
 var func = function(){
     var a = 1;
+    var btemp = arguments != null && arguments.count > 0 ? arguments[0] : 0;
     return function(){
         a++;
         console.log(a);
@@ -9,3 +10,29 @@ var f = func();
 f();//2
 f();//3
 f();//4
+
+//html
+<html>
+<body>
+<div>1</div>
+<div>2</div>
+<div>3</div>
+<div>4</div>
+<div>5</div>
+<script>
+var nodes = document.getElementsByTagName('div');
+for(var i = 0, len = nodes.length; i < len; i++){
+    // error.
+    nodes[i].onclick = function(){
+         console.log(i);
+    }
+    //right.
+    (function(i){
+        nodes[i].onclick = function(){
+           console.log(i);
+        }
+    })(i)
+}
+</script>
+</body>
+</html>
