0766049c8db2aac23632f463f87e8e7dbc2e2c36
jack <xu090411@qq.com>
2016-05-20 13:01:32 +0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -187,6 +187,37 @@ extent.call();//2
  </script>
 </body>
 </html>
+//
+var Tv = {
+    open:function(){
+        console.log("open tv.");
+    },
+    close:function(){
+        console.log("close tv.");
+    }
+};
+var createCommand = function(receiver){
+    var execute = function(){
+        return receiver.open();
+    }
+    var undo = function(){
+        return receiver.close();
+    }
+    return {
+        execute:execute,
+        undo:undo
+    }
+};
+var setCommand = function(command){
+    document.getElementById('execute').onclick = function(){
+        command.execute();
+    }
+    document.getElementById('undo').onclick = function(){
+        command.undo();
+    }
+};
+setCommand(createCommand(Tv));
+
 
 
 
