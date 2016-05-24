aaae77e537dcd02e157327553f8aea034237aaea
jack <xu090411@qq.com>
2016-05-24 22:54:30 +0800

    Update domscript.js

--- domscript.js
+++ domscript.js
@@ -11,6 +11,17 @@ function prepareGallery(){
     }
 }
 function showPic(whichPic){
-    //...
+    if(!document.getElementById("placeholder")) return false;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if(placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src", source);
+    if(document.getElementById("description")){
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if(description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = test;
+        }
+    }
     return true;
 }
