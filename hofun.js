5e8c678299c3f254c5d6fa131ea61dd484522fa8
jack <xu090411@qq.com>
2016-05-28 12:27:31 +0800

    Update hofun.js

--- hofun.js
+++ hofun.js
@@ -117,5 +117,23 @@ cost(100);
 cost(200);
 cost(300);
 console.log(monthlyCost);// 600
-
+//
+var cost = (function(){
+   var args = [];
+   return function(){
+      if(arguments.length === 0){
+         var money = 0;
+         for(var i = 0, l = args.lenght; i < l; i++){
+            money += args[i];
+         }
+         return money;
+      }else{
+         [].push.apply(args, arguments);
+      }
+   }
+})();
+cost(100);
+cost(200);
+cost(300);
+console.log(cost());// 600
 
