7e1a548139d995757d8389dd64e0aedf2744e8cf
jack <xu090411@qq.com>
2016-05-30 19:27:19 +0800

    Update hofun.js

--- hofun.js
+++ hofun.js
@@ -163,4 +163,27 @@ cost(200);
 cost(300);
 console.log(cost()); // 600
 //uncurrying
-
+var obj1 = {
+   name:'sven'
+};
+var obj2 = {
+   getName:function(){
+      return this.name;
+   }
+};
+console.log(obj2.getName.call(obj1));// sven
+(function(){
+   Array.prototype.push.call(arguments, 4);
+})(1,2,3);// [1,2,3,4]
+Function.prototype.uncurrying = function(){
+   var self = this;
+   return function(){
+      var obj = Array.prototype.shift.call(arguments);
+      return self.apply(obj, arguments);
+   }
+};
+var push = Array.prototype.push.uncurrying();
+(function(){
+   push(arguments, 4);
+   console.log(arguments);//[1,2,3,4]
+})(1,2,3);
