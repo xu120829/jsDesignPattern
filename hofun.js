2ba68f968d95169ddb90e07cb5eaa3bf0d152222
jack <xu090411@qq.com>
2016-06-03 18:10:42 +0800

    Update hofun.js

--- hofun.js
+++ hofun.js
@@ -272,5 +272,24 @@ var renderFriendList = function(data){
    }
 };
 renderFriendList(ary);
+//
+var timeChunk = function(ary, fn, count){
+   var obj, t;
+   var len = ary.length;
+   var start = function(){
+      for(var i = 0; i < Math.min(count || 1, ary.length); i++){
+         var obj = ary.shift();
+          fn(obj);
+      }
+   };
+   return function(){
+      t = setInterval(function(){
+         if(ary.length === 0){
+            return clearInterval(t);
+         }
+         start();
+      }, 200);
+   };
+};
 
 
