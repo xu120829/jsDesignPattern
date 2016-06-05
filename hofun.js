f4386c86eb4f06ef175960424fd936b4fca6ba78
jack <xu090411@qq.com>
2016-06-05 15:49:45 +0800

    Update hofun.js

--- hofun.js
+++ hofun.js
@@ -324,5 +324,32 @@ var addEvent = (function(){
       }
    }
 })();
+//
+<html>
+<body>
+   <div id="div1">click me binding event.</div>
+   <script>
+      var addEvent = function(elem, type, handler){
+         if(window.addEventListener){
+            addEvent = function(elem, type, handler){
+               elem.addEventListener(type, handler, false);
+            }
+         }else if(window.attachEvent){
+            addEvent = function(elem, type, handler){
+               elem.attachEvent('on' + type, handler);
+            }
+         }
+         addEvent(elem, type, handler);
+      };
+      var div = document.getElementById('div1');
+      addEvent(div, 'click', function(){
+         console.log(1);
+      });
+      addEvent(div, 'click', function(){
+         console.log(2);
+      });
+   </script>
+</body>
+</html>
 
 
